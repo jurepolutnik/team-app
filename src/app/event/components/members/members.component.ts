42f38343f1afe63d27af39b840022c0fcb06145c
Jure Polutnik <jure.polutnik@gmail.com>
2016-11-16 13:57:35 +0100

    Quick fixes - style, libs, minor features, etc.

--- src/app/event/components/members/members.component.ts
+++ src/app/event/components/members/members.component.ts
@@ -3,6 +3,7 @@ import { TeamService } from './../../../team/services/team.service';
 import { Event } from './../../../team/models/event';
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit, Input } from '@angular/core';
+import * as moment from 'moment'
 
 declare var $: any;
 
@@ -22,7 +23,19 @@ export class MembersComponent implements OnInit {
     this.memberName = localStorage.getItem('user');
   }
 
+  toDate () {
+    let now = new Date();
+    let to = this.event.date;
+    let diff = moment(to).diff(moment(now))
+    let d = moment.duration(diff);
+    if (d.days() === 1) return `${d.days()} day ${d.hours()} hours ${d.minutes()} minutes`
+    if (d.days() > 1) return `${d.days()} days ${d.hours()} hours ${d.minutes()} minuets`
+    if (d.hours() > 0) return `${d.hours()} hours ${d.minutes()} minutes`
+    return `${d.hours()} hours ${d.minutes()} minutes`
+  }
+
   submit(): void {
+    if (!this.memberName) return;
     localStorage.setItem('user', this.memberName)
     this.teamService.addMember(this.event, this.memberName);
     this.clear();
